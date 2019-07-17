cbe79a8f2bd90e9f9c58a6a6955c57ffd1aaed35
Dan Doozan <dandoozan@gmail.com>
2019-07-16 21:27:29 -0700

    Clean up _main.js

--- src/js/contentScripts/_main.js
+++ src/js/contentScripts/_main.js
@@ -1,38 +1,22 @@
 import { getContentScriptObject } from '../utils';
 
-function setTitle(title) {
-    document.title = title;
-}
-
-function urlContainsFragment(url, fragment) {
-    return url.indexOf(fragment) > -1;
-}
-
 (async function main() {
-    //NOTE: "pageModule" is available as a global variable because
-    //each of the page scripts are built as a "library" by webpack, which exports the
-    //module as a global variable.  I had to do it this way because chrome extensions don't
-    //allow loading actual es modules as content scripts yet.  In the future, each page script
-    //should be an es module, which means I'll be able to dynamically "import()" them here (instead
-    //of using a global variable), but until then, this is the best solution I could come up with.
-
     let url = window.location.href;
     let oldTitle = document.title;
 
-    //first, get the config for this page
-    let { fragment, title: newTitle } = await getContentScriptObject(url) || {};
+    let { fragment, newTitle } =
+        (await getContentScriptObject(url)) || {};
 
-    //then, set the title
     //NOTE: I have to check for the 'fragment' part of the url (ie. the part
     //after the '#') here b/c the manifest 'matches' url thing doesn't take
     //into account the url fragment
-    if (!fragment || urlContainsFragment(url, fragment)) {
+    if (!fragment || url.indexOf(fragment) > -1) {
         //handle case where title is regex replacement
         if (Array.isArray(newTitle)) {
             let [from, to] = newTitle;
-            setTitle(oldTitle.replace(new RegExp(from), to));
+            document.title = oldTitle.replace(new RegExp(from), to);
         } else {
-            setTitle(newTitle);
+            document.title = newTitle;
         }
     }
 })();
