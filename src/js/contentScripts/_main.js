85f7a30a9a688a57c8304d56e4a5695a34af38b1
Dan Doozan <dandoozan@gmail.com>
2019-10-05 09:00:34 -0700

    Run prettier across all files

--- src/js/contentScripts/_main.js
+++ src/js/contentScripts/_main.js
@@ -1,22 +1,21 @@
 import { getContentScriptObject } from '../helpers/chrome';
 
 (async function main() {
-    let url = window.location.href;
-    let oldTitle = document.title;
+  let url = window.location.href;
+  let oldTitle = document.title;
 
-    let { fragment, newTitle } =
-        (await getContentScriptObject(url)) || {};
+  let { fragment, newTitle } = (await getContentScriptObject(url)) || {};
 
-    //NOTE: I have to check for the 'fragment' part of the url (ie. the part
-    //after the '#') here b/c the manifest 'matches' url thing doesn't take
-    //into account the url fragment
-    if (!fragment || url.indexOf(fragment) > -1) {
-        //handle case where title is regex replacement
-        if (Array.isArray(newTitle)) {
-            let [from, to] = newTitle;
-            document.title = oldTitle.replace(new RegExp(from), to);
-        } else {
-            document.title = newTitle;
-        }
+  //NOTE: I have to check for the 'fragment' part of the url (ie. the part
+  //after the '#') here b/c the manifest 'matches' url thing doesn't take
+  //into account the url fragment
+  if (!fragment || url.indexOf(fragment) > -1) {
+    //handle case where title is regex replacement
+    if (Array.isArray(newTitle)) {
+      let [from, to] = newTitle;
+      document.title = oldTitle.replace(new RegExp(from), to);
+    } else {
+      document.title = newTitle;
     }
+  }
 })();
--- src/js/options.js
+++ src/js/options.js
@@ -1,40 +1,44 @@
-import { readFromStorage, writeToStorage, clearStorage } from './helpers/chrome';
+import {
+  readFromStorage,
+  writeToStorage,
+  clearStorage,
+} from './helpers/chrome';
 
 function displayMessage(elId, msg) {
-    var el = document.getElementById(elId);
-    el.textContent = msg;
-    setTimeout(function() {
-        el.textContent = '';
-    }, 750);
+  var el = document.getElementById(elId);
+  el.textContent = msg;
+  setTimeout(function() {
+    el.textContent = '';
+  }, 750);
 }
 
 // Saves options to chrome.storage
 async function saveOptions() {
-    //todo: handle case when the textarea doesnt contain valid json
-    await writeToStorage({
-        options: JSON.parse(document.getElementById('optionsTextArea').value),
-    });
+  //todo: handle case when the textarea doesnt contain valid json
+  await writeToStorage({
+    options: JSON.parse(document.getElementById('optionsTextArea').value),
+  });
 
-    // Update status to let user know options were saved
-    displayMessage('saveStatus', 'Options saved.');
+  // Update status to let user know options were saved
+  displayMessage('saveStatus', 'Options saved.');
 }
 
 // Restores preferences stored in chrome.storage
 async function restoreOptions() {
-    let { options } = await readFromStorage({ options: {} });
+  let { options } = await readFromStorage({ options: {} });
 
-    //display the options on the page
-    document.getElementById('optionsTextArea').value = JSON.stringify(
-        options,
-        null,
-        '    '
-    );
+  //display the options on the page
+  document.getElementById('optionsTextArea').value = JSON.stringify(
+    options,
+    null,
+    '    '
+  );
 }
 
 async function clearOptions() {
-    await clearStorage();
-    await restoreOptions();
-    displayMessage('clearStatus', 'Options cleared.');
+  await clearStorage();
+  await restoreOptions();
+  displayMessage('clearStatus', 'Options cleared.');
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
