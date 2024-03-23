a56d37d309601288c92d9927d28abfe2327b85c2
Dan Doozan <dandoozan@gmail.com>
2024-03-22 17:44:28 -0700

    Fix XD and ASU

--- src/js/contentScripts/ASU.js
+++ src/js/contentScripts/ASU.js
@@ -1,16 +1,19 @@
 let originalTitle = document.title;
 let url = window.location.href;
 
-//set the title to the first letters of the left-most subdomain. Examples:
-//"https://dev-bios-ws2.ws.asu.edu/scope/plankton-profiles" -> "dbw"
-//"https://dev-ddzn-bios.ws.asu.edu/" -> "ddb"
-//"https://mymultidev-bios-ws2.ws.asu.edu/" -> "mbw"
+//prevent changing the title a second time if it was already changed
+if (!originalTitle.includes('[')) {
+  //set the title to the first letters of the left-most subdomain. Examples:
+  //"https://dev-bios-ws2.ws.asu.edu/scope/plankton-profiles" -> "dbw"
+  //"https://dev-ddzn-bios.ws.asu.edu/" -> "ddb"
+  //"https://mymultidev-bios-ws2.ws.asu.edu/" -> "mbw"
 
-let domain = url.split('/')[2];
-let subdomain = domain.split('.')[0];
-let subdomainParts = subdomain.split('-');
-let abbreviation = subdomainParts.map((part) => part[0]).join('');
+  let domain = url.split('/')[2];
+  let subdomain = domain.split('.')[0];
+  let subdomainParts = subdomain.split('-');
+  let abbreviation = subdomainParts.map((part) => part[0]).join('');
 
-document.title = `[${abbreviation}]${originalTitle}`;
+  document.title = `[${abbreviation}]${originalTitle}`;
 
-console.log(`*** Changed title to: ${document.title}`);
+  console.log(`*** Changed title to: ${document.title}`);
+}
--- src/js/contentScripts/XD.js
+++ src/js/contentScripts/XD.js
@@ -2,11 +2,14 @@ import { text } from '../helpers/dom';
 
 let originalTitle = document.title;
 
-//wait a little bit to give the [data-stringtype="artboardName"] element time to render (it's not rendered on page load)
-setTimeout(() => {
-  document.title = `${text(
-    '[data-stringtype="artboardName"]'
-  )} - ${originalTitle}`;
-
-  console.log(`*** Changed title to: ${document.title}`);
-}, 1000);
+//prevent changing the title a second time if it was already changed
+if (!originalTitle.includes(' - ')) {
+  //wait a little bit to give the [data-stringtype="artboardName"] element time to render (it's not rendered on page load)
+  setTimeout(() => {
+    let artboardName = text('[data-stringtype="artboardName"]');
+    if (artboardName) {
+      document.title = `${artboardName} - ${originalTitle}`;
+      console.log(`*** Changed title to: ${document.title}`);
+    }
+  }, 1000);
+}
