b3c24fc369dc55b66bb24ce3f7e3945b9149dc08
Dan Doozan <dandoozan@gmail.com>
2024-03-26 11:29:13 -0700

    XD: fix it

--- src/js/contentScripts/XD.js
+++ src/js/contentScripts/XD.js
@@ -1,15 +1,16 @@
 import { text } from '../helpers/dom';
 
-let originalTitle = document.title;
+//wait a little bit to give the [data-stringtype="artboardName"] element time to render (it's not rendered on page load)
+setTimeout(() => {
+  let originalTitle = document.title;
 
-//prevent changing the title a second time if it was already changed
-if (!originalTitle.includes(' - ')) {
-  //wait a little bit to give the [data-stringtype="artboardName"] element time to render (it's not rendered on page load)
-  setTimeout(() => {
-    let artboardName = text('[data-stringtype="artboardName"]');
-    if (artboardName) {
-      document.title = `${artboardName} - ${originalTitle}`;
-      console.log(`*** Changed title to: ${document.title}`);
-    }
-  }, 1000);
-}
+  //if I had previously set the title, reset it (this is needed when navigating back to a page that was previously modified)
+  if (originalTitle.includes(' - ')) {
+    originalTitle = originalTitle.split(' - ')[1];
+  }
+  let artboardName = text('[data-stringtype="artboardName"]');
+  document.title = artboardName
+    ? `${artboardName} - ${originalTitle}`
+    : originalTitle;
+  console.log(`*** Changed title to: ${document.title}`);
+}, 1000);
